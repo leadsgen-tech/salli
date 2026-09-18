d7c4d0e0d1a02b8de127e1744e132d7508f49bc9
nabil11111 <68223398+nabil11111@users.noreply.github.com>
2026-09-18 19:21:40 +0530

    chore(release): v0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "lk.salli.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.4.2"
+        versionCode = 8
+        versionName = "0.5.0"
         manifestPlaceholders["salliAppLabel"] = "@string/app_name"
     }
 
